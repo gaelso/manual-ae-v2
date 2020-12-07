c87ddd19dbc2c905e81792ffc7712c3058e008d3
gael.sola <gaelsola@hotmail.fr>
2020-12-07 11:52:36 +0700

    Added bookdown style files for TeX and html outputs

---
+++ R-make-book.R
@@ -0,0 +1,39 @@
+## Thailand emission factor update 2020
+## March 2020
+## Gael Sola, FAO
+
+## ++++++
+## This script sources R scripts and makes the report with bookdown::render_book()
+## ++++++
+
+## Source R scripts
+source("R/00-libs.R")
+
+source("R/01-photo-edit.R")
+
+
+## Make report
+
+## DOCX
+# bookdown::render_book(
+#   input = "index.Rmd",
+#   output_format = "bookdown::word_document2",
+#   output_file = paste0("Manual-AE-", format(Sys.time(), format = "%Y-%m-%d-%H%M%S"), ".docx"),
+#   output_dir = "Manual-AE",
+#   output_options = list(reference_docx = "ref-empty.docx")
+# )
+
+## PDF -- For pdf install webshot and webshot::install_phantomjs() 
+bookdown::render_book(
+  input = "index.Rmd",
+  output_format = "bookdown::pdf_book",
+  output_file = paste0("Manual-AE-", format(Sys.time(), format = "%Y-%m-%d-%H%M%S"), ".pdf"),
+  output_dir = "Manual-AE"
+  )
+
+## HTML - GITBOOK
+bookdown::render_book(
+  input = "index.Rmd",
+  output_format = "bookdown::gitbook",
+  output_dir = paste0("Manual-AE/Manual-AE-gitbook-", format(Sys.time(), format = "%Y-%m-%d-%H%M%S"))
+)
